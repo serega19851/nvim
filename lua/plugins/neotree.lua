887da7eda09e1adda1ea9f042426b1b11c3de22f
sa <sergeyyyy1985.an@gmail.com>
2023-10-20 11:10:53 +0300

    Created nvim settings

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,28 @@
+require'window-picker'.setup({
+    autoselect_one = false,
+    include_current = false,
+    filter_rules = {
+        bo = {
+            filetype = {'neo-tree', "neo-tree-popup", "notify"},
+            buftype = {'terminal', "quickfix"}
+        }
+    },
+    other_win_hl_color = '#e35e4f'
+})
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+vim.fn.sign_define("DiagnosticSignError",
+                   {text = " ", texthl = "DiagnosticSignError"})
+vim.fn.sign_define("DiagnosticSignWarn",
+                   {text = " ", texthl = "DiagnosticSignWarn"})
+vim.fn.sign_define("DiagnosticSignInfo",
+                   {text = " ", texthl = "DiagnosticSignInfo"})
+vim.fn.sign_define("DiagnosticSignHint",
+                   {text = "", texthl = "DiagnosticSignHint"})
+
+require("neo-tree").setup({
+   filesystem = {
+              filtered_items = {
+                visible = true, -- when true, they will just be displayed differently than normal items
+            }
+}})
